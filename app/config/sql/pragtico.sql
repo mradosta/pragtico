48a839003a7d6ee701571c3cfdb06738d3e45e87
mradosta <mradosta@7ca50b70-d50e-461b-8d46-8ecfda400a84>
2008-11-13 15:07:18 +0000

    agrego script de incializacion de DB

---
+++ app/config/sql/pragtico.sql
@@ -0,0 +1,6 @@
+# Copyright 2007-2008, Pragmatia de RPB S.A.
+#
+# Licensed under The MIT License
+# Redistributions of files must retain the above copyright notice.
+# http://www.opensource.org/licenses/mit-license.php The MIT License
+
