f08d11de9a06389dc75bb64a9faf6126306450a6
mradosta <mradosta@7ca50b70-d50e-461b-8d46-8ecfda400a84>
2008-11-13 15:20:31 +0000

    bugfix y comentarios (prueba que habia quedado mal)

--- app/controllers/components/history.php
+++ app/controllers/components/history.php
@@ -1,35 +1,94 @@
-<?php 
+<?php
 /**
+ * History Component.
+ * Se encarga resolver la navegacion para atras.
+ * 
+ * Me baso parcialmente en el trabajo de Studio Sipak
+ * website: http://webdesign.janenanneriet.nl
+ *
+ * PHP versions 5
+ *
+ * @filesource
+ * @copyright		Copyright 2007-2008, Pragmatia de RPB S.A.
+ * @link			http://www.pragmatia.com
+ * @package			pragtico
+ * @subpackage		app.controllers.components
+ * @since			Pragtico v 1.0.0
+ * @version			$Revision$
+ * @modifiedby		$LastChangedBy$
+ * @lastmodified	$Date$
+ * @author      	Martin Radosta <mradosta@pragmatia.com>
+ */
+
+ /**
  * Tamanio maximo del array que contendra el historial de navegacion.
  */
 define('MAX_HISTORY', 10);
-/*
- * HistoryComponent: User navigation history
- * @author: Studio Sipak
- * @website: http://webdesign.janenanneriet.nl
- * @license: MIT
- * @version: 0.1
- * */
-class HistoryComponent extends Object
-{
-    var $__historia = array();
-    var $started = false;
-    var $controller;
 
+/**
+ * La clase encapsula la logica necesaria para resolver la navegacion.
+ *
+ * @package		pragtico
+ * @subpackage	app.controllers.components
+ */
+class HistoryComponent extends Object {
+    
+	
+/**
+ * Guarda las paginas por las que fue pasando.
+ *
+ * @var array
+ * @access private
+ */
+	private $__historia = array();
+	
+	
+/**
+ * Indica si ya fue instanciado el component.
+ *
+ * @var boolean
+ * @access private
+ */
+    var $__started= false;
+	
+	
+/**
+ * El Controller que instancio el component.
+ *
+ * @var array
+ * @access public
+ */
+	var $controller;
+
+	
+/**
+ * Inicializa el Component para usar en el controller.
+ *
+ * @param object $controller Una referencia al controller que esta instanciando el component.
+ * @return void
+ * @access public
+ */
     function startup(&$controller) {
     
         /**
         * Prevengo que entre mas de una vez.
         */
-        if(!$this->started) {
-        
-            $this->started = true;
+        if(!$this->__started) {
+            $this->__started = true;
             $this->controller = $controller;
-            
 			$this->_addUrl();
         }
     }
 
+	
+/**
+ * Vuelve a una pagina visitada anterior.
+ *
+ * @param integer $pos Cuantas paginas atras quiero volverme.
+ * @return void
+ * @access public
+ */
+	
     function goBack($pos = 1) {
         if(is_numeric($pos)) {
 	        $history = array();
@@ -50,13 +109,24 @@ class HistoryComponent extends Object
         }
     }
 
+	
+/**
+ * Me muesta la historia guardada.
+ *
+ * @return void
+ * @access public
+ */
     function show() {
         return $this->__historia;
     }
 
-	/**
-	* Agrega una url al stack del history, que nunca se usara.
-	*/
+	
+/**
+ * Agrega una url al stack del history, que nunca se usara. 
+ *
+ * @return void
+ * @access public
+ */
     function addFakeUrl() {
 		if(count($this->controller->Session->read('historia')) == MAX_HISTORY) {
 			array_shift($this->__historia);
@@ -65,18 +135,21 @@ class HistoryComponent extends Object
 		$this->controller->Session->write('historia', $this->__historia);
     }
 
-    function _addUrl() {
+    
+/**
+ * Agrega una url al stack del history,
+ *
+ * @return void
+ * @access public
+ */
+	function _addUrl() {
 		$url = $this->controller->referer();
-		Debugger::output($url); 
     	if(empty($url)) {
     		return;
     	}
+		
 		$this->__historia = $this->controller->Session->read('historia');
 		$cantidad = count($this->__historia);
-		/**
-		* Prevengo que se inserte en la history dos veces el mismo.
-		* Por ejemplo, cuando un validate no valida, etc.
-		*/
 		
 		/**
 		* Cuando abro una lov o un desglose ajax, o cancelo no guardo esto en el history.
@@ -90,6 +163,10 @@ class HistoryComponent extends Object
 			return;
 		}
 		
+		/**
+		* Prevengo que se inserte en la history dos veces el mismo.
+		* Por ejemplo, cuando un validate no valida, etc.
+		*/
 		if($url != $this->__historia[$cantidad - 1]) {
 			if($cantidad == MAX_HISTORY) {
 				array_shift($this->__historia);
@@ -100,66 +177,8 @@ class HistoryComponent extends Object
 			return;
 		}
 		
-		/*
-		file_put_contents("/tmp/historia.txt", "=========================", FILE_APPEND);
-		foreach($this->__historia as $k=>$v) {
-			file_put_contents("/tmp/historia.txt", "\n(" . $k . ")" . $v, FILE_APPEND);
-		}
-		file_put_contents("/tmp/historia.txt", "\n\n=========================", FILE_APPEND);
-		*/
 		$this->controller->Session->write('historia', $this->__historia);
     }
-	
-	
-    function _addUrl_old($params) {
-    	if(empty($params['url']['url'])) {
-    		return;
-    	}
-		$this->__historia = $this->controller->Session->read('historia');
-		$url = trim("/" . $params['url']['url']);
-		$cantidad = count($this->__historia);
-		/**
-		* Prevengo que se inserte en la history dos veces el mismo.
-		* Por ejemplo, cuando un validate no valida, etc.
-		*/
-		
-		//if(empty($this->__historia)) {
-		//	$this->__historia[] = $url;
-		//}
-		//else {
-			/**
-			* Cuando abro una lov o un desglose ajax, o cancelo no guardo esto en el history.
-			*/
-			if(    (!empty($this->controller->data['Form']['accion']) && $this->controller->data['Form']['accion'] === "cancelar")
-				|| (!empty($this->controller->params['named']['layout']) && $this->controller->params['named']['layout'] === "lov")
-				|| (!empty($this->controller->data['Formulario']['layout']) && $this->controller->data['Formulario']['layout'] === "lov")
-				|| (!empty($this->controller->data['Formulario']['layout']) && $this->controller->data['Formulario']['layout'] === "lov")
-				|| (!empty($this->controller->params['action']) && ($this->controller->params['action'] == "listable" || $this->controller->params['action'] == "descargar"))
-				|| (!empty($this->controller->params['isAjax']))) {
-				return;
-			}
-			
-			$parsedUrl = Router::parse($url);
-			$parsedHistoryUrl = Router::parse($this->__historia[$cantidad - 1]);
-			$actual = $parsedUrl['controller'] . "/" . $parsedUrl['action'];
-			$history = $parsedHistoryUrl['controller'] . "/" . $parsedHistoryUrl['action'];
-			if($actual != $history) {
-				if($cantidad == MAX_HISTORY) {
-					array_shift($this->__historia);
-				}
-				$this->__historia[] = $url;
-			}
-			else {
-				return;
-			}
-		//}
-		file_put_contents("/tmp/historia.txt", "=========================", FILE_APPEND);
-		foreach($this->__historia as $k=>$v) {
-			file_put_contents("/tmp/historia.txt", "\n(" . $k . ")" . $v, FILE_APPEND);
-		}
-		file_put_contents("/tmp/historia.txt", "\n\n=========================", FILE_APPEND);
-		$this->controller->Session->write('historia', $this->__historia);
-    }
-	
+
 }
 ?>
