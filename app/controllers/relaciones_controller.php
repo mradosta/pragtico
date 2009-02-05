e41dec2807d6290cf45e31a10c523d3f72e3499d
mradosta <mradosta@7ca50b70-d50e-461b-8d46-8ecfda400a84>
2009-02-05 18:06:12 +0000

    Add default search criteria

--- app/controllers/relaciones_controller.php
+++ app/controllers/relaciones_controller.php
@@ -26,17 +26,28 @@ class RelacionesController extends AppController {
 
 
 /**
+ * Set default search condition to active relations.
+ */
+	function index() {
+		if (empty($this->data['Condicion.Relacion-estado'])) {
+			$this->data['Condicion']['Relacion-estado'] = 'Activa';
+		}
+		return parent::index();
+	}
+
+
+/**
  * areas_relacionado.
  */
 	function areas_relacionado($id) {
 		$c=0;
-		foreach ($this->Relacion->Area->find("list", array("fields"=>array("Area.nombre"), "conditions"=>array("Area.empleador_id"=>$id))) as $k=>$v) {
+		foreach ($this->Relacion->Area->find('list', array('fields' => array('Area.nombre'), 'conditions'=>array('Area.empleador_id' => $id))) as $k => $v) {
 			$areas[$c]['optionValue'] = $k;
 			$areas[$c]['optionDisplay'] = $v;
 			$c++;
 		}
-		$this->set("data", $areas);
-		$this->render("../elements/json");
+		$this->set('data', $areas);
+		$this->render('../elements/json');
 	}
 
 
@@ -45,15 +56,15 @@ class RelacionesController extends AppController {
  */
 	function recibos_relacionado($id) {
 		$c=0;
-		foreach ($this->Relacion->Empleador->Recibo->find("list", array("fields"=>array("Recibo.nombre"), "conditions"=>array("Recibo.empleador_id"=>$id))) as $k=>$v) {
+		foreach ($this->Relacion->Empleador->Recibo->find('list', array('fields' => array('Recibo.nombre'), 'conditions'=>array('Recibo.empleador_id' => $id))) as $k => $v) {
 			$recibos[$c]['optionValue'] = $k;
 			$recibos[$c]['optionDisplay'] = $v;
 			$c++;
 		}
-		$this->set("data", $recibos);
-		$this->render("../elements/json");
+		$this->set('data', $recibos);
+		$this->render('../elements/json');
 	}
-	
+
 /**
  * Add.
  */
@@ -62,13 +73,13 @@ class RelacionesController extends AppController {
 		* Si esta grabando y selecciona un recibo del empleador, agrego a la relacion laboral,
 		* los conceptos que posea ese recibo.
 		*/
-        if (!empty($this->data['Relacion']['recibo_id']) && !empty($this->data['Form']['accion']) && $this->data['Form']['accion'] == "grabar") {
+        if (!empty($this->data['Relacion']['recibo_id']) && !empty($this->data['Form']['accion']) && $this->data['Form']['accion'] == 'grabar') {
         	$recibo = $this->Relacion->Empleador->Recibo->findById($this->data['Relacion']['recibo_id']);
         	foreach ($recibo['RecibosConcepto'] as $v) {
-        		$relacionesConcepto[] = array("concepto_id"=>$v['concepto_id']);
+        		$relacionesConcepto[] = array('concepto_id'=>$v['concepto_id']);
         	}
         	unset($this->data['Relacion']['recibo_id']);
-	        $this->data = am($this->data, array("RelacionesConcepto"=>$relacionesConcepto));
+	        $this->data = array_merge($this->data, array('RelacionesConcepto'=>$relacionesConcepto));
         }
         parent::add();
 	}
@@ -79,10 +90,10 @@ class RelacionesController extends AppController {
  * Muestra via desglose los Pagos asociados a la relacion laboral.
  */
 	function pagos($id) {
-		$this->Relacion->contain(array("Pago"));
+		$this->Relacion->contain(array('Pago'));
 		$this->data = $this->Relacion->read(null, $id);
 	}
-	
+
 
 /**
  * Vacaciones.
@@ -92,34 +103,34 @@ class RelacionesController extends AppController {
 		$this->data = $this->Relacion->read(null, $id);
 	}
 
-	
+
 /**
  * Ausencias.
  * Muestra via desglose las Ausencias asociadas a la relacion laboral.
  */
 	function ausencias($id) {
-		$this->Relacion->contain(array("Ausencia.AusenciasMotivo"));
+		$this->Relacion->contain(array('Ausencia.AusenciasMotivo'));
 		$result = $this->Relacion->read(null, $id);
 		$this->data = $result;
 	}
 
-	
+
 /**
  * Conceptos.
  * Muestra via desglose los Conceptos asociados a la relacion laboral.
  */
 	function conceptos($id) {
-		$this->Relacion->contain(array("RelacionesConcepto.Concepto"));
+		$this->Relacion->contain(array('RelacionesConcepto.Concepto'));
 		$this->data = $this->Relacion->read(null, $id);
 	}
 
-	
+
 /**
  * Ropas.
  * Muestra via desglose la ropa entregada a la relacion laboral.
  */
 	function ropas($id) {
-		$this->Relacion->contain("Ropa");
+		$this->Relacion->contain('Ropa');
 		$this->data = $this->Relacion->read(null, $id);
 	}
 
@@ -129,7 +140,7 @@ class RelacionesController extends AppController {
  * Muestra via desglose las horas asignadas a una relacion.
  */
 	function horas($id) {
-		$this->Relacion->contain("Hora");
+		$this->Relacion->contain('Hora');
 		$this->data = $this->Relacion->read(null, $id);
 	}
 
@@ -139,7 +150,7 @@ class RelacionesController extends AppController {
  * Muestra via desglose los descuentos de una relacion.
  */
 	function descuentos($id) {
-		$this->Relacion->contain("Descuento");
+		$this->Relacion->contain('Descuento');
 		$this->data = $this->Relacion->read(null, $id);
 	}
 
@@ -149,23 +160,23 @@ class RelacionesController extends AppController {
  * Muestra via desglose el detalle de los descuentos realizados.
  */
 	function descuentos_detalle($id) {
-		$this->Relacion->Descuento->contain(array("DescuentosDetalle"));
+		$this->Relacion->Descuento->contain(array('DescuentosDetalle'));
 		$this->data = $this->Relacion->Descuento->read(null, $id);
-		$this->render("../descuentos/detalles");
+		$this->render('../descuentos/detalles');
 	}
 
  /**
  * PasarAHistorico.
  * Al no poder eliminar una relacion laboral le cambia el estado a historico.
 	function pasarAHistorico($id) {
-		if ($this->Relacion->save(array("Relacion"=>array('id' => $id, "estado" => "Historica")))) {
-			$this->Session->setFlash("Se paso al historico la relacion laboral correctamente.", 'ok');
+		if ($this->Relacion->save(array('Relacion'=>array('id' => $id, 'estado' => 'Historica')))) {
+			$this->Session->setFlash('Se paso al historico la relacion laboral correctamente.', 'ok');
 		}
 		else {
-			$this->Session->setFlash("No pudo pasarse a historico la relacion laboral.", 'error');
+			$this->Session->setFlash('No pudo pasarse a historico la relacion laboral.', 'error');
 		}
 		$this->History->goBack();
-	}	
+	}
  */
 }
 ?>
