a4f280ef562ae24f7659519bc674833c8b89844c
mradosta <mradosta@7ca50b70-d50e-461b-8d46-8ecfda400a84>
2009-07-23 21:56:55 +0000

    Clean up a little the code

--- app/controllers/service_controller.php
+++ app/controllers/service_controller.php
@@ -50,55 +50,24 @@ class ServiceController extends AppController {
     var $components = array('Soap');
 
 
-
     function probar_consumir() {
         /*
         $client = new MSSoapClient("http://upsoft.dyndns.org/manager2Saldos/webservice.asmx?WSDL",
             array("trace"=>1,
                     'uri'=>"http://localhost/servicioWeb"));
         */
-
         $soapClient = new MSSoapClient("http://192.168.1.151/WsManager2/webService.asmx?wsdl",
             array("trace"=>1,
                     'uri'=>"http://localhost/servicioWeb"));        
         try {
 
-            //strGrupo
-            //strCodigo
-            //strCUIT
-            //$request = new SoapVar(array('strCUIT'=>"30641966645"), SOAP_ENC_OBJECT);
-            //$request = new SoapVar(array('strCUIT' => "20131933992"), SOAP_ENC_OBJECT);
             $request = new SoapVar(array('strGrupo' => '30-59083010-7', 'strCodigo' => '792001', 'strCUIT' => '30-54285183-6'), SOAP_ENC_OBJECT);
         
             d($soapClient->limites($request));
         } catch(SoapFault $soapFault) {
-            //d($soapFault);
-            //echo $client->__getLastRequest();
             echo "Request :<br>", $client->__getLastRequest(), "<br>";
             echo "Response :<br>", $client->__getLastResponse(), "<br>";
         }
-        //echo print_r($soapClient->limites);
-        die;
-    
-    /*
-        $soapClient = new
-            SoapClient(
-                "http://upsoft.dyndns.org/manager2Saldos/webservice.asmx?WSDL", array('trace'=>1, 'soap_version'=>SOAP_1_2)
-            );
-        try {
-            $retorno['wsdl'] = "";
-            $retorno['retorno'] = $soapClient->limites("30641966645");
-        }
-        catch (SoapFault $soapFault) {
-            //var_dump($soapFault);
-            echo "Request :<br>", $soapClient->__getLastRequest(), "<br>";
-            echo "Response :<br>", $soapClient->__getLastResponse(), "<br>";
-            die();
-            //d("X");
-        }
-    */
-        $this->set("pruebas", $retorno);
-        $this->render("probar_cliente");
     }
 
     
@@ -112,31 +81,20 @@ class ServiceController extends AppController {
     }
 
     function probar_cliente() {
-        //d(utf8_decode("o'connor &#xE1;&#xE9;&#xED;&#xF3;&#xFA;"));
-        //d($this->Manager2Service->facturacion(0));
-        //d($this->Manager2Service->pagos(100));
-        //d($this->Manager2Service->anulaciones_pagos(0));
-        //d(inflector::variable("empleador_id"));
-        //$this->autoRender = false;
         $soapClient = new
             SoapClient(
                 router::url("/", true) . "service/wsdl/manager2", array('trace'=> 1)
             );
         try {
             $retorno['wsdl'] = $this->Soap->getWSDL("manager2", 'call');
-            //$retorno['retorno'] = $soapClient->empleadores(25200);
+            $retorno['retorno'] = $soapClient->empleadores(0);
             //$retorno['retorno'] = $soapClient->facturacion(71);
-            $retorno['retorno'] = $soapClient->pagos(347);
+            //$retorno['retorno'] = $soapClient->pagos(347);
             //$retorno['retorno'] = $soapClient->anulaciones_pagos(0);
-            //d($soapClient->facturacion(1) . ". Se ejecuto OK");
-            //d($soapClient->hola("MARTIN") . ". Se ejecuto OK");
-            //d($soapClient->divide(10, 2) . ". Se ejecuto OK");
         }
         catch (SoapFault $soapFault) {
-            //var_dump($soapFault);
             echo "Request :<br>", $soapClient->__getLastRequest(), "<br>";
             echo "Response :<br>", $soapClient->__getLastResponse(), "<br>";
-            d("X");
         }
         $this->set("pruebas", $retorno);
     }
