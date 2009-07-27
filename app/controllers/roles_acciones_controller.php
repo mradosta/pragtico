734fd2ebb9f953ec72bc1631268c41934903075a
mradosta <mradosta@7ca50b70-d50e-461b-8d46-8ecfda400a84>
2009-07-27 19:56:28 +0000

    Fix multiple actions per role save

--- app/controllers/roles_acciones_controller.php
+++ app/controllers/roles_acciones_controller.php
@@ -25,42 +25,43 @@
  */
 class RolesAccionesController extends AppController {
 
-	function add() {
+	function save() {
 		/**
 		* Detecto si viene de un addRapido.
 		*/
-		if (!empty($this->data['Form']['tipo']) && $this->data['Form']['tipo'] == "addRapido" && !empty($this->data['RolesAccion']['rol_id'])) {
+		if (!empty($this->data['Form']['tipo']) && $this->data['Form']['tipo'] == 'addRapido' && !empty($this->data['RolesAccion']['rol_id'])) {
 
-			if ($this->data['Form']['accion'] === "grabar") {
+			if ($this->data['Form']['accion'] === 'grabar') {
 				$rolId = $this->data['RolesAccion']['rol_id'];
 				unset($this->data['RolesAccion']['rol_id']);
 				$this->RolesAccion->begin();
-				$this->RolesAccion->deleteAll(array("RolesAccion.rol_id"=>$rolId));
-				$cc = $c = 0;
-				foreach ($this->data['RolesAccion'] as $acciones) {
-					foreach ($acciones as $accionId) {
-						$cc++;
-						$save = array("RolesAccion"=>array("rol_id"=>$rolId, "accion_id"=>$accionId));
-						$this->RolesAccion->create($save);
-						if ($this->RolesAccion->save($save)) {
-							$c++;
-						}
-					}
+				$this->RolesAccion->deleteAll(array('RolesAccion.rol_id'=>$rolId));
+                $saveAll = array();
+				foreach ($this->data['RolesAccion'] as $actions) {
+                    if (!empty($actions)) {
+                        foreach ($actions as $accionId) {
+                            $saveAll[] = array('RolesAccion' =>
+                                array('rol_id' => $rolId, 'accion_id' => $accionId));
+                        }
+                    }
 				}
-				if ($c === $cc) {
-					$this->Session->setFlash("Los cambios se guardaron correctamente.", "ok");
-					$this->RolesAccion->commit();
-				}
-				else {
-					$this->Session->setFlash("Los cambios no pudieron guardarse.", "error");
-					$this->RolesAccion->rollback();
-				}
+
+                if (!empty($saveAll)) {
+                    if ($this->RolesAccion->saveAll($saveAll)) {
+                        $this->Session->setFlash('Los cambios se guardaron correctamente.', 'ok');
+                        $this->RolesAccion->commit();
+                    } else {
+                        $this->Session->setFlash('Los cambios no pudieron guardarse.', 'error');
+                        $this->RolesAccion->rollback();
+                    }
+                } else {
+                    /** Must commit deleted actions */
+                    $this->Session->setFlash('Las acciones se eliminaron correctamente.', 'ok');
+                    $this->RolesAccion->commit();
+                }
 			}
-			$this->redirect("../roles/index");
 		}
-		else {
-			parent::add();
-		}
+        $this->History->goBack();
 	}
 	
 
@@ -70,32 +71,30 @@ class RolesAccionesController extends AppController {
 	function add_rapido() {
 		if (!empty($this->params['named']['RolesAccion.rol_id']) && is_numeric($this->params['named']['RolesAccion.rol_id'])) {
 			$data = $controladores = $acciones = array();
-			$this->RolesAccion->contain("Accion");
+			$this->RolesAccion->contain('Accion');
 			
-			$r = $this->RolesAccion->find("all", array("conditions"=>array("RolesAccion.rol_id"=>$this->params['named']['RolesAccion.rol_id'])));
+			$r = $this->RolesAccion->find('all', array('conditions'=>array('RolesAccion.rol_id'=>$this->params['named']['RolesAccion.rol_id'])));
 			if (!empty($r)) {
-				$r = Set::extract("/Accion/id", $r);
+				$r = Set::extract('/Accion/id', $r);
 			}
 			
-			foreach ($this->RolesAccion->Accion->Controlador->find("all") as $controlador) {
+			foreach ($this->RolesAccion->Accion->Controlador->find('all') as $controlador) {
 				$controladores[$controlador['Controlador']['id']] = $controlador['Controlador']['nombre'];
 				foreach ($controlador['Accion'] as $accion) {
 					$acciones[$accion['id']] = $accion['nombre'];
 					if (!empty($r) && in_array($accion['id'], $r)) {
 						$data[$controlador['Controlador']['id']][$accion['id']] = true;
-					}
-					else {
+					} else {
 						$data[$controlador['Controlador']['id']][$accion['id']] = false;
 					}
 				}
 			}
-			$this->set("data", $data);
-			$this->set("dataControladores", $controladores);
-			$this->set("dataAcciones", $acciones);
-			$this->set("rolId", $this->params['named']['RolesAccion.rol_id']);
-		}
-		else {
-			$this->Session->setFlash("Debe seleccionar un Rol.", "error");
+			$this->set('data', $data);
+			$this->set('dataControladores', $controladores);
+			$this->set('dataAcciones', $acciones);
+			$this->set('rolId', $this->params['named']['RolesAccion.rol_id']);
+		} else {
+			$this->Session->setFlash('Debe seleccionar un Rol.', 'error');
 			$this->History->goBack(2);
 		}
 	}
