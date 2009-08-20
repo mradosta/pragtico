5059b7809dcc2c38ad20a6a76f888c27ad9f728b
mradosta <mradosta@7ca50b70-d50e-461b-8d46-8ecfda400a84>
2009-08-20 01:10:45 +0000

    Update PHPExcel version (commit 1)

--- app/vendors/pragmatia/formulas.php
+++ app/vendors/pragmatia/formulas.php
@@ -53,23 +53,21 @@ class Formulas {
  */
     function __construct() {
 		/** PHPExcel_Calculation */
-		if (!class_exists('PHPExcel_Calculation')) {
-			App::import('Vendor', 'Calculation', true, array(APP . 'vendors' . DS . 'PHPExcel' . DS . 'Classes' . DS . 'PHPExcel'), 'Calculation.php');
-		}
+        App::import('Vendor', 'Calculation', true, array(APP . 'vendors' . DS . 'PHPExcel' . DS . 'Classes' . DS . 'PHPExcel'), 'Calculation.php');
 		$this->__objPHPExcel = new PHPExcel();
 	}
 
 
 /**
- * __cleanUp the formula.
+ * Remove spaces and "strange" characters from the formula.
  *
- * @param string $formula. The formula to resolv.
+ * @param string $formula. The formula to be resolved.
  * @return string The ready to use formula.
  * @access private
  */
 	function __cleanUp($formula) {
 		/** Replace spaces in formulas to unify criterias.*/
-		$formula = preg_replace('/\s*([=|,|\(|\)])\s*/', '$1', $formula);
+		$formula = preg_replace('/\s*([=|<|>|,|\(|\)])\s*/', '$1', $formula);
 		$formula = preg_replace('/(\d)\s{0,}\/\s{0,}(\d)/', '$1/$2', $formula);
 		$formula = preg_replace('/[^[:print:]]/', '', $formula);
 		if (substr($formula, 0, 1) !== '=') {
@@ -78,72 +76,31 @@ class Formulas {
 		return $formula;
 	}
 
-	
-	function __cleanUpDate($strings, $formula) {
-		foreach (array_unique($strings[0]) as $k => $string) {
-			if ($strings[1][$k] + $strings[2][$k] + $strings[3][$k] === 0) {
-				$formula = str_replace($string, '', $formula);
-			} else {
-				$formula = str_replace($string, sprintf('date(%d, %d, %d)', $strings[1][$k], $strings[2][$k], $strings[3][$k]), $formula);
-			}
-		}
-		return $formula;
-	}
-	
 
 /**
  * Resolv the formula.
  *
  * @param string $formula. The formula to resolv.
- * @return mixed Value for the resolved formula. N/A when errors in formula.
+ * @return mixed The result of the resolved formula.
  * @access public
  */
 	function resolver($formula) {
+        
 		$cellId = 0;
-
 		$formula = $this->__cleanUp($formula);
-
-		/** Partial solution to avoid PHPExcel unnecesary formula part evaluation */
-		// http://phpexcel.codeplex.com/WorkItem/View.aspx?WorkItemId=9447
-		$parts = explode(',', $formula);
-		if (!empty($parts)) {
-			foreach($parts as $k => $part) {
-				$parts[$k] = preg_replace('/([\d\.]+\/0)/', '#N/A', $part);
-			}
-			$formula = implode(',', $parts);
-		}
-		
-		/** Replace Mysql dates to PHPExcel dates */
-		if (preg_match_all("/date\('(\d\d\d\d)-(\d\d)-(\d\d)'\)/", $formula, $strings)) {
-			$formula = $this->__cleanUpDate($strings, $formula);
-		}
-		if (preg_match_all("/'(\d\d\d\d)-(\d\d)-(\d\d)'/", $formula, $strings)) {
-			$formula = $this->__cleanUpDate($strings, $formula);
-		}
-		if (preg_match_all("/(\d\d\d\d)-(\d\d)-(\d\d)/", $formula, $strings)) {
-			$formula = $this->__cleanUpDate($strings, $formula);
+        $formula = preg_replace("/isblank\(\'?0000\-00\-00\'?\)/", 'true', $formula);
+        $formula = preg_replace("/isblank\(\'?\d\d\d\d\-\d\d\-\d\d\'?\)/", 'false', $formula);
+        
+		if (preg_match_all("/date\(\'?(\d\d\d\d)-(\d\d)-(\d\d)\'?\)/", $formula, $strings)) {
+            foreach (array_unique($strings[0]) as $k => $string) {
+                $formula = str_replace($string, sprintf('date(%d, %d, %d)', $strings[1][$k], $strings[2][$k], $strings[3][$k]), $formula);
+            }
+		} elseif (preg_match_all("/\'?(?!\")(\d\d\d\d)-(\d\d)-(\d\d)\'?(?!\")/", $formula, $strings)) {
+            foreach (array_unique($strings[0]) as $k => $string) {
+                $formula = str_replace($string, sprintf('"%d-%d-%d"', $strings[1][$k], $strings[2][$k], $strings[3][$k]), $formula);
+            }
 		}
 
-
-		/** Convert group functions arguments to values in columns */
-		if (preg_match_all("/(.*)([min|max|sum|average]+)\(([[0-9]\,]+)\)/Ui", $formula, $partes)) {
-			if (!empty($partes[3])) {
-				$formulaParcialRecontruida = null;
-				foreach ($partes[3] as $k => $valores) {
-					$tmpValores = explode(',', $valores);
-					$rangoInferior = 'A' . ($cellId + 1);
-					foreach ($tmpValores as $valor) {
-						$cellId++;
-						$this->__objPHPExcel->getActiveSheet()->setCellValue('A' . $cellId, (int)$valor);
-					}
-					$rangoSuperior = 'A' . $cellId;
-					$tmpPartes[] = $partes[1][$k] . $partes[2][$k] . '(' . $rangoInferior . ':' . $rangoSuperior . ')';
-				}
-				$formula = str_replace(implode('', $partes[0]), implode('', $tmpPartes), $formula);
-				$formula = str_replace('/' . implode('', $partes[0]) . '/', implode('', $tmpPartes), $formula);
-			}
-		}
-		
 		$this->__cellId++;
 		$formula = str_replace('\'', '"', $formula);
 		$this->__objPHPExcel->getActiveSheet()->setCellValue('ZZ' . $this->__cellId, $formula);
