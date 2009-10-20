03457079fc47ad5d847c9e27c5dcc94cf0fe6ad6
mradosta <mradosta@7ca50b70-d50e-461b-8d46-8ecfda400a84>
2009-10-20 19:57:04 +0000

    Add new report

--- app/controllers/familiares_controller.php
+++ app/controllers/familiares_controller.php
@@ -32,5 +32,37 @@ class FamiliaresController extends AppController {
     );
 
 
+    var $helpers = array('Documento');
+
+    function reporte_familiares() {
+
+        if (!empty($this->data['Formulario']['accion']) && $this->data['Formulario']['accion'] === 'generar') {
+
+            if (!empty($this->data['Condicion']['Bar-empleador_id'])) {
+                $this->Familiar->Trabajador->Relacion->Behaviors->detach('Permisos');
+                $this->Familiar->Trabajador->Relacion->recursive = -1;;
+                $conditions['Familiar.trabajador_id'] = Set::extract('/Relacion/trabajador_id', $this->Familiar->Trabajador->Relacion->find('all', array('conditions' => array('Relacion.empleador_id' => explode('**||**', $this->data['Condicion']['Bar-empleador_id'])))));
+            }
+            
+            if (!empty($this->data['Condicion']['Bar-trabajador_id'])) {
+                $conditions['Familiar.trabajador_id'] = explode('**||**', $this->data['Condicion']['Bar-trabajador_id']);
+            }
+
+            if (empty($conditions)) {
+                $this->Session->setFlash('Debe seleccionar por lo menos un empleador o un trabajador.', 'error');
+                $this->History->goBack();
+            }
+            
+            $this->Familiar->Behaviors->detach('Permisos');
+            $this->set('data', $this->Familiar->find('all', array(
+                'contain'       => 'Trabajador',
+                'order'         => array('Trabajador.cuil', 'Familiar.nombre'),
+                'conditions'    => $conditions)));
+                        
+            $this->set('fileFormat', $this->data['Condicion']['Bar-file_format']);
+        }
+    }
+    
+
 }
 ?>
