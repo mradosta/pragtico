4eac85cf83bbf7c470e3f7bc3a1e4eb4b1c3b118
mradosta <mradosta@7ca50b70-d50e-461b-8d46-8ecfda400a84>
2009-11-10 19:55:56 +0000

    Continue updating code to avoid the use of complicated tables. Ref issue #12

--- app/controllers/convenios_conceptos_controller.php
+++ app/controllers/convenios_conceptos_controller.php
@@ -32,108 +32,70 @@ class ConveniosConceptosController extends AppController {
         )
     );
 
-	function add() {
-		/**
-		* Detecto si viene de un addRapido.
-		*/
-		if (!empty($this->data['Form']['tipo']) && $this->data['Form']['tipo'] == 'addRapido' && !empty($this->data['ConveniosConcepto']['convenio_id'])) {
 
-			if ($this->data['Form']['accion'] == 'grabar') {
-				$registros = explode('*||*', $this->data['Form']['valores_derecha']);
+    function save() {
+        if (!empty($this->data['Form']['tipo'])
+            && $this->data['Form']['tipo'] == 'addRapido'
+            && !empty($this->data['ConveniosConcepto']['convenio_id'])) {
 
-				foreach ($registros as $registro) {
-					$seleccionados[] = array_shift(explode('|', $registro));
-				}
-				
-				$conceptosSeleccionados = $this->ConveniosConcepto->Concepto->find('list', array('recursive'=>-1, 'conditions'=>array('Concepto.codigo'=>$seleccionados)));
-				$conceptosConvenio = $this->ConveniosConcepto->find('list', array('fields' => 'ConveniosConcepto.concepto_id', 'contain' => 'Concepto', 'conditions'=>array('ConveniosConcepto.convenio_id'=>$this->data['ConveniosConcepto']['convenio_id'])));
-				
-				$quitar = array_diff($conceptosConvenio, $conceptosSeleccionados);
-				$agregar = array_diff($conceptosSeleccionados, $conceptosConvenio);
-				$this->ConveniosConcepto->begin();
-				$c = 0;
-				foreach ($quitar as $k => $v) {
-					if ($this->ConveniosConcepto->del($k)) {
-						$c++;
-					}
-				}
-				
-				$save['ConveniosConcepto']['convenio_id'] = $this->data['ConveniosConcepto']['convenio_id'];
-				foreach ($agregar as $k => $v) {
-					$this->ConveniosConcepto->create();
-					$save['ConveniosConcepto']['concepto_id'] = $v;
-					$this->ConveniosConcepto->set($save);
-					if ($this->ConveniosConcepto->save($save)) {
-						$c++;
-					}
-				}
-				if ($c == count($agregar) + count($quitar)) {
-					$this->Session->setFlash('La operacion se realizo con exito.', 'ok', array('warnings'=>$this->{$this->modelClass}->getWarning()));
-					$this->ConveniosConcepto->commit();
-				} else {
-					$dbError = $this->{$this->modelClass}->getError();
-					$this->Session->setFlash('Los cambios no pudieron guardarse.', 'error', array('errores'=>$dbError));
-					$this->ConveniosConcepto->rollBack();
-				}
-			}
-			$this->History->goBack(2);
-		}
-		else {
-			parent::add();
-		}
-	}
+            $assignedConcepts = $this->__getAssignedConcepts($this->data['ConveniosConcepto']['convenio_id'], true);
 
+            foreach ($this->data['Concepto'] as $k => $v) {
+                list($conceptId, $conceptCode) = explode('|', $k);
+                if ($v == 1 && !in_array($conceptCode, $assignedConcepts)) {
+                    $add[] = array('ConveniosConcepto' => array(
+                        'convenio_id'  => $this->data['ConveniosConcepto']['convenio_id'],
+                        'concepto_id'   => $conceptId));
+                } elseif ($v == 0 && in_array($conceptCode, $assignedConcepts)) {
+                    $del[] = $conceptId;
+                }
+            }
+
+            if (!empty($add)) {
+                $this->ConveniosConcepto->saveAll($add);
+            }
+            if (!empty($del)) {
+                $this->ConveniosConcepto->deleteAll(array(
+                    'convenio_id'  => $this->data['ConveniosConcepto']['convenio_id'],
+                    'concepto_id'   => $del));
+            }
+            $this->Session->setFlash('La operacion se realizo con exito.', 'ok');
+            $this->redirect(array('controller' => 'convenios', 'action' => 'index'));
+        } else {
+            return parent::save();
+        }
+    }
 
 /**
 * Permite realizar un add mediante tablas fromto.
 */
-	function add_rapido() {
+    function add_rapido() {
 
-		if (!empty($this->passedArgs['ConveniosConcepto.convenio_id'])) {
-			$this->ConveniosConcepto->Convenio->contain(array('Concepto'));
-			$convenio = $this->ConveniosConcepto->Convenio->findById($this->passedArgs['ConveniosConcepto.convenio_id']);
+        if (!empty($this->passedArgs['ConveniosConcepto.convenio_id'])) {
+            $agreement = $this->__getAssignedConcepts($this->passedArgs['ConveniosConcepto.convenio_id']);
+            $assignedConcepts = Set::extract('/Concepto/codigo', $agreement);
+            $concepts = $this->ConveniosConcepto->Concepto->find('all',
+                array(  'recursive'  => -1,
+                        'order'      => array('Concepto.nombre')));
 
-			if (!empty($convenio['Concepto'])) {
-				$conceptosAsignados = Set::extract('/Concepto', $convenio);
-				$conceptosAsignadosCodigos = Set::extract('/Concepto/codigo', $conceptosAsignados);
-			} else {
-				$conceptosAsignados = $conceptosAsignadosCodigos = array();
-			}
-			$conceptosNoAsignados = $this->ConveniosConcepto->Concepto->find('all',
-				array(	'recursive'	=>	-1,
-						'conditions'=>
-							array('NOT' => array('Concepto.codigo' => $conceptosAsignadosCodigos))));
-			
-			$this->set('convenio', $convenio);
-			$this->set('datosIzquierda', $conceptosNoAsignados);
-			$this->set('datosDerecha', $conceptosAsignados);
-		} else {
-			$this->Session->setFlash('Debe seleccionar un Convenio.', 'error');
-			$this->History->goBack(2);
-		}
-	}
+            $this->set('agreement', $agreement);
+            $this->set('concepts', $concepts);
+            $this->set('assignedConcepts', $assignedConcepts);
+        } else {
+            $this->Session->setFlash('Debe seleccionar un Empleador.', 'error');
+            $this->History->goBack(2);
+        }
+    }
 
-	function actualizarTablaIzquierda() {
-		if (isset($this->params['named']['partialText'])) {
-			$this->params['named']['partialText'] = str_replace('[EXPANSOR]', '%', str_replace('[SPACE]', ' ', $this->params['named']['partialText']));
-			$condiciones = array('Concepto.nombre like'=>$this->params['named']['partialText']);
-			unset($this->params['named']['partialText']);
-		}
-		if (isset($this->params['named']['selectedId'])){
-			$condiciones = array('Concepto.id'=>$this->params['named']['selectedId']);
-			unset($this->params['named']['selectedId']);
-		}
 
-		$conceptos = $this->ConveniosConcepto->Concepto->find('all', array('conditions'=>$condiciones));
-		$data = array();
-		foreach ($conceptos as $v) {
-			$data[$v['Concepto']['id']] = $v['Concepto']['nombre'];
-		}
-		$tablaSiemple = $this->Util->generarCuerpoTablaSimple($data);
-		$this->set('cuerpo', $tablaSiemple['cuerpo']);
-		$this->set('encabezados', $tablaSiemple['encabezados']);
-		$this->render('../elements/tablas_from_to/tabla');
-	}
-
+    function __getAssignedConcepts($agreementId, $extract = false) {
+        $this->ConveniosConcepto->Convenio->contain(array('Concepto'));
+        $agreement = $this->ConveniosConcepto->Convenio->findById($agreementId);
+        if ($extract === true) {
+            return Set::extract('/Concepto/codigo', $agreement);
+        } else {
+            return $agreement;
+        }
+    }
 }
 ?>
