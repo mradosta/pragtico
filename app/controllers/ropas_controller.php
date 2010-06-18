b0626335c90cc0ede0c749a86d20be1e746fcbff
mradosta <mradosta@7ca50b70-d50e-461b-8d46-8ecfda400a84>
2010-06-18 13:41:42 +0000

    Remove deprecated code

--- app/controllers/ropas_controller.php
+++ app/controllers/ropas_controller.php
@@ -26,12 +26,26 @@
 class RopasController extends AppController {
 
 
+	var $helpers = array("Pdf");
+
+
+
 /**
  * Prendas.
  * Muestra via desglose las prendas entregadas. 
  */
    	function prendas($id) {
-		$this->Ropa->contain('RopasDetalle');
+		$this->Ropa->contain("RopasDetalle");
+		$this->data = $this->Ropa->read(null, $id);
+   	}
+
+
+/**
+ * imprimirOrden.
+ * Genera una orden que el empleado pueda retirar la ropa.
+ */
+   	function imprimirOrden($id) {
+		$this->layout = 'pdf';
 		$this->data = $this->Ropa->read(null, $id);
    	}
 }
